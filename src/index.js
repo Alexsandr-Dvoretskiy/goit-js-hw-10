6517afefa41543dae47730a470d8de35c4b6504b
Alexsandr-Dvoretskiy <alexdor72742@gmail.com>
2022-12-04 08:19:12 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
-
 import debounce from 'lodash.debounce';
-
 import './css/styles.css';
-
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
